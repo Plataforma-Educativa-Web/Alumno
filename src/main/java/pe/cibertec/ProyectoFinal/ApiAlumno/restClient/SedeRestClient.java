71e33fb19e35512cefdb7d1944bc56835321323c
Victor Borja <vborja2424@gmail.com>
2023-11-24 14:43:56 -0500

    Terminado

--- src/main/java/pe/cibertec/ProyectoFinal/ApiAlumno/restClient/SedeRestClient.java
+++ src/main/java/pe/cibertec/ProyectoFinal/ApiAlumno/restClient/SedeRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pe.cibertec.ProyectoFinal.ApiAlumno.entity.Sede;
 
-@FeignClient(value = "sede-service", url = "http://localhost:8084/")
+@FeignClient(value = "sede-service", url = "http://localhost:8086/")
 
 public interface SedeRestClient {
 
